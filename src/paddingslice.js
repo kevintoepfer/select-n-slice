2f3e75e341e1075724121311829a4991e36250f4
kevintoepfer <53897272+kevintoepfer@users.noreply.github.com>
2021-04-16 14:35:51 +0200

    Update paddingslice.js
    Fixed a bug where an empty slice got created when padding input was aborted.

--- src/paddingslice.js
+++ src/paddingslice.js
@@ -2,6 +2,7 @@ var sketch = require('sketch')
 
 
 export default function(context, value) {
+
   var document = context.document;
   var currentPage = document.currentPage();
   var selection = context.selection;
@@ -21,28 +22,28 @@ export default function(context, value) {
         return
       } else {
         slicePadding = value;
+
+        // get selected objects
+        var handler = context.document.eventHandlerManager().normalHandler();
+        var whatUserSelected = handler.selectedRect();
+
+        // get size and shape
+        var originalLocation = whatUserSelected.origin;
+        var originalSize = whatUserSelected.size;
+
+        // set slice size
+        var newSlice = MSSliceLayer.new();
+        var sliceFrame = newSlice.frame();
+
+        // apply slice size
+        sliceFrame.setX(originalLocation.x - slicePadding);
+        sliceFrame.setY(originalLocation.y - slicePadding);
+        sliceFrame.setWidth(originalSize.width + slicePadding * 2);
+        sliceFrame.setHeight(originalSize.height + slicePadding * 2);
+
+        // add slice
+        document.currentPage().addLayers([newSlice]);
       }
     }
   );
-
-  // get selected objects
-  var handler = context.document.eventHandlerManager().normalHandler();
-  var whatUserSelected = handler.selectedRect();
-
-  // get size and shape
-  var originalLocation = whatUserSelected.origin;
-  var originalSize = whatUserSelected.size;
-
-  // set slice size
-  var newSlice = MSSliceLayer.new();
-  var sliceFrame = newSlice.frame();
-
-  // apply slice size
-  sliceFrame.setX(originalLocation.x - slicePadding);
-  sliceFrame.setY(originalLocation.y - slicePadding);
-  sliceFrame.setWidth(originalSize.width + slicePadding * 2);
-  sliceFrame.setHeight(originalSize.height + slicePadding * 2);
-
-  // add slice
-  document.currentPage().addLayers([newSlice]);
 };
